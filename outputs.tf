5c526a4763274141f48e9309d1713f67fd755831
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "eventhub_namespace_disaster_recovery_configs" {
-  description = "All eventhub_namespace_disaster_recovery_config resources"
-  value       = azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs
-}
 output "eventhub_namespace_disaster_recovery_configs_name" {
-  description = "List of name values across all eventhub_namespace_disaster_recovery_configs"
-  value       = [for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : v.name]
+  description = "Map of name values across all eventhub_namespace_disaster_recovery_configs, keyed the same as var.eventhub_namespace_disaster_recovery_configs"
+  value       = { for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : k => v.name }
 }
 output "eventhub_namespace_disaster_recovery_configs_namespace_name" {
-  description = "List of namespace_name values across all eventhub_namespace_disaster_recovery_configs"
-  value       = [for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : v.namespace_name]
+  description = "Map of namespace_name values across all eventhub_namespace_disaster_recovery_configs, keyed the same as var.eventhub_namespace_disaster_recovery_configs"
+  value       = { for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : k => v.namespace_name }
 }
 output "eventhub_namespace_disaster_recovery_configs_partner_namespace_id" {
-  description = "List of partner_namespace_id values across all eventhub_namespace_disaster_recovery_configs"
-  value       = [for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : v.partner_namespace_id]
+  description = "Map of partner_namespace_id values across all eventhub_namespace_disaster_recovery_configs, keyed the same as var.eventhub_namespace_disaster_recovery_configs"
+  value       = { for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : k => v.partner_namespace_id }
 }
 output "eventhub_namespace_disaster_recovery_configs_resource_group_name" {
-  description = "List of resource_group_name values across all eventhub_namespace_disaster_recovery_configs"
-  value       = [for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : v.resource_group_name]
+  description = "Map of resource_group_name values across all eventhub_namespace_disaster_recovery_configs, keyed the same as var.eventhub_namespace_disaster_recovery_configs"
+  value       = { for k, v in azurerm_eventhub_namespace_disaster_recovery_config.eventhub_namespace_disaster_recovery_configs : k => v.resource_group_name }
 }
 
